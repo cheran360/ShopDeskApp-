79e1862fee00d14f5949a0665a79548d09ab7aea
cheran <cherankumar.548@gmail.com>
2022-04-06 21:04:31 +0530

    ipc response should be cleared

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const electron = require("electron");
 const url = require("url");
 const path = require("path");
 
-const { app, BrowserWindow, Menu } = electron;
+const { app, BrowserWindow, Menu, ipcMain } = electron;
 
 let mainWindow;
 let addWindow;
@@ -51,6 +51,12 @@ function createAddWindow() {
     addWindow = null;
   });
 }
+// Catch item:add
+ipcMain.on("item:add", function (e, item) {
+  console.log(item);
+  mainWindow.webContents.send("item:add", item);
+  addWindow.close();
+});
 
 // Create menu template, Basically an array of objs
 const mainMenuTemplate = [
